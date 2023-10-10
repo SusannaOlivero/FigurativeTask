e58ff3730b9d1f86d52c805baa78946a7f56c15d
SusannaOlivero <131966758+SusannaOlivero@users.noreply.github.com>
2023-10-10 11:20:31 -0500

    Update run_translation.py

--- examples/pytorch/translation/run_translation.py
+++ examples/pytorch/translation/run_translation.py
@@ -53,7 +53,6 @@ from transformers.utils.versions import require_version
 import torch
 import pdb
 torch.backends.cuda.matmul.allow_tf32 = True
-CUDA_LAUNCH_BLOCKING=1
 
 # Will error if the minimal version of Transformers is not installed. Remove at your own risks.
 check_min_version("4.13.0.dev0")
@@ -354,18 +353,21 @@ def main():
         revision=model_args.model_revision,
         use_auth_token=True if model_args.use_auth_token else None,
     )
+
+    
+    ### DEVICE ###
     
     #device_ids = [0,1,2,3]
     #device_map = {device_ids[0]: list(range(0, 6)),device_ids[1]: list(range(6,12)),device_ids[2]: list(range(12,18)),device_ids[3]: list(range(18,24))}
     #model.parallelize(device_map)
 
-    device_ids = [0,1]
+    device_ids = [0]
     if model_args.model_name_or_path == 't5-small':
         device_map = {device_ids[0]: list(range(0, 6))}
     elif model_args.model_name_or_path=='t5-base' or model_args.model_name_or_path=="figurative-nlp/t5-figurative-generation" or model_args.model_name_or_path=="figurative-nlp/t5-figurative-paraphrase":
         device_map = {device_ids[0]: list(range(0, 12))}
     else:
-        device_map = {device_ids[0]: list(range(0, 6)),device_ids[1]: list(range(6,24))}
+        device_map = {device_ids[0]: list(range(0, 24))}
     model.parallelize(device_map)
 
     model.resize_token_embeddings(len(tokenizer))
